3db14e53812ce3efe093f7737e3fdb65b3642caa
Dipanshu Raj <108553225+dpsdmh@users.noreply.github.com>
2022-10-10 19:25:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import '../styles/App.css';
+import {useState} from 'react';
+
 const App = () => {
-  
+  const [alternate,setAlternate] = useState(false);
+  const handleClick = () =>{
+  if(alternate == false){
+  setAlternate(true)
+  }
+    else{
+    setAlternate(false)
+  }
+  }
   return (
     <div id="main">
-
+     <h1 id='marco-polo'>{alternate?"Polo":"Marco"}</h1>
+    <button id="marco-polo-toggler" onClick={handleClick}>{ alternate?"Marco":"Polo"}</button>
     </div>
   )
 }
